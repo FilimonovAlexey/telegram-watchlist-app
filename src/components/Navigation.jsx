8b7adda3abae7eddf79b2901d70417d0bed4428e
Alexey <alexeyf08@gmail.com>
2025-01-28 02:30:54 +0700

    Обновлено меню

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,23 +4,27 @@ import { Link } from "react-router-dom";
 export default function Navigation({ canEdit }) {
   return (
     <nav className="navigation">
-      <Link to="/" className="nav-button home-button">
-        🏠
-      </Link>
-      <Link to="/movies" className="nav-button">
-        Фильмы
-      </Link>
-      <Link to="/series" className="nav-button">
-        Сериалы
-      </Link>
-      <Link to="/history" className="nav-button history-button">
-        История
-      </Link>
-      {canEdit && (
-        <Link to="/add" className="nav-button add-button">
-          Добавить
+      <div className="nav-row">
+        <Link to="/" className="nav-button home-button">
+          🏠
         </Link>
-      )}
+        <Link to="/movies" className="nav-button">
+          Фильмы
+        </Link>
+        <Link to="/series" className="nav-button">
+          Сериалы
+        </Link>
+      </div>
+      <div className="nav-row secondary">
+        <Link to="/history" className="nav-button history-button">
+          История
+        </Link>
+        {canEdit && (
+          <Link to="/add" className="nav-button add-button">
+            Добавить
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
